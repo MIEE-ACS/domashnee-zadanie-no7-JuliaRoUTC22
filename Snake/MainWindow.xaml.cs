246723dc23bcd7aaf7115ed4812850db9d8112bd
Julia Rozhkova <71033750+JuliaRoUTC22@users.noreply.github.com>
2020-12-22 05:23:24 +0300

    Выполнила базовый upgrade "отравленное яблоко 3".

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
-        //Поле на котором живет змея
+        //поле, на котором живет змея
         Entity field;
         // голова змеи
         Head head;
@@ -28,11 +28,20 @@ namespace Snake
         List<PositionedEntity> snake;
         // яблоко
         Apple apple;
+        // отравленное яблоко
+        Poisoned_Apple_3 poisoned_apple_3;
         //количество очков
         int score;
-        //таймер по которому 
+        //для случайных чисел
+        static Random rand = new Random();
+        //таймер, по которому осуществляется движение
         DispatcherTimer moveTimer;
-        
+        // переменная, отвечающая за состояние здоровья змейки
+        bool health = false;
+        //таймер, по которому происходит снятие очков во время отравления
+        DispatcherTimer poisoningTimer;
+
+
         //конструктор формы, выполняется при запуске программы
         public MainWindow()
         {
@@ -40,34 +49,46 @@ namespace Snake
             
             snake = new List<PositionedEntity>();
             //создаем поле 300х300 пикселей
-            field = new Entity(600, 600, "pack://application:,,,/Resources/snake.png");
+            field = new Entity(600, 600, "pack://application:,,,/Resources/field.png");
 
-            //создаем таймер срабатывающий раз в 300 мс
+            //делаем текст с предупреждением об отравлении невидимым
+            lbl_poison.Visibility = Visibility.Hidden;
+
+            //создаем таймер, срабатывающий раз в 290 мс
             moveTimer = new DispatcherTimer();
-            moveTimer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+            moveTimer.Interval = new TimeSpan(0, 0, 0, 0, 290);
             moveTimer.Tick += new EventHandler(moveTimer_Tick);
-            
+
+            //создаем таймер, срабатывающий раз в 1 с
+            poisoningTimer = new DispatcherTimer();
+            poisoningTimer.Interval = new TimeSpan(0, 0, 0, 0, 1000);
+            poisoningTimer.Tick += new EventHandler(poisoningTimer_Tick);
+ 
         }
 
-        //метод перерисовывающий экран
+        //метод, перерисовывающий экран
         private void UpdateField()
         {
             //обновляем положение элементов змеи
             foreach (var p in snake)
             {
-                Canvas.SetTop(p.image, p.y);
-                Canvas.SetLeft(p.image, p.x);
+              Canvas.SetTop(p.image, p.y);
+              Canvas.SetLeft(p.image, p.x);
             }
 
             //обновляем положение яблока
             Canvas.SetTop(apple.image, apple.y);
             Canvas.SetLeft(apple.image, apple.x);
-            
+
+            //обновляем положение отравленного яблока
+             Canvas.SetTop(poisoned_apple_3.image, poisoned_apple_3.y);
+             Canvas.SetLeft(poisoned_apple_3.image, poisoned_apple_3.x);
+               
             //обновляем количество очков
             lblScore.Content = String.Format("{0}000", score);
         }
 
-        //обработчик тика таймера. Все движение происходит здесь
+        //обработчик тика таймера. Всё движение происходит здесь.
         void moveTimer_Tick(object sender, EventArgs e)
         {
             //в обратном порядке двигаем все элементы змеи
@@ -84,7 +105,10 @@ namespace Snake
                 {
                     //мы проиграли
                     moveTimer.Stop();
+                    poisoningTimer.Stop();
+
                     tbGameOver.Visibility = Visibility.Visible;
+                    btn_start.Visibility = Visibility.Visible;
                     return;
                 }
             }
@@ -94,25 +118,94 @@ namespace Snake
             {
                 //мы проиграли
                 moveTimer.Stop();
+                poisoningTimer.Stop();
+
                 tbGameOver.Visibility = Visibility.Visible;
+                btn_start.Visibility = Visibility.Visible;
                 return;
             }
 
+
+            //проверяем, что голова змеи врезалась в отравленное яблоко
+            if (head.x == poisoned_apple_3.x && head.y == poisoned_apple_3.y)
+            {
+                health = false;
+
+                //делаем текст с предупреждением об отравлении
+                lbl_poison.Visibility = Visibility.Visible;
+
+                //съедаем отравленное яблоко
+                canvas1.Children.Remove(poisoned_apple_3.image);
+
+                //запускаем таймер
+                poisoningTimer.Start();
+
+                //двигаем отравленное яблоко на новое место
+                poisoned_apple_3.move();
+                
+            }
+
+          
             //проверяем, что голова змеи врезалась в яблоко
             if (head.x == apple.x && head.y == apple.y)
             {
+                health = true;
+
+                //убираем текст с предупреждением об отравлении
+                lbl_poison.Visibility = Visibility.Hidden;
+
+                //останавливаем таймер
+                poisoningTimer.Stop();
+
                 //увеличиваем счет
                 score++;
+
+                //убираем отравленное яблоко
+                canvas1.Children.Remove(poisoned_apple_3.image);
+
                 //двигаем яблоко на новое место
                 apple.move();
+
                 // добавляем новый сегмент к змее
                 var part = new BodyPart(snake.Last());
                 canvas1.Children.Add(part.image);
                 snake.Add(part);
+
+                // создаем отравленное яблоко с вероятностью 0,05
+                if (rand.Next(1, 101) == 11 || rand.Next(1, 101) == 32 || rand.Next(1, 101) == 47 || rand.Next(1, 101) == 56 || rand.Next(1, 101) == 79) 
+                {
+                    health = false;
+                    canvas1.Children.Add(poisoned_apple_3.image);
+
+                    //двигаем отравленное яблоко на новое место
+                    poisoned_apple_3.move();
+                }
             }
+
             //перерисовываем экран
             UpdateField();
         }
+
+
+        void poisoningTimer_Tick(object sender, EventArgs e)
+        {
+            if (!health)
+            {
+                //уменьшаем счет 
+                score -= 1;
+            }
+
+            if (head.x == apple.x && head.y == apple.y)
+            {
+                poisoningTimer.Stop();
+            
+                canvas1.Children.Add(apple.image);
+                apple.move();
+
+                UpdateField();
+            }
+        }
+
 
         // Обработчик нажатия на кнопку клавиатуры
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -134,30 +227,47 @@ namespace Snake
             }
         }
 
-        // Обработчик нажатия кнопки "Start"
-        private void button1_Click(object sender, RoutedEventArgs e)
+        // обработчик нажатия кнопки "Start"
+        private void btn_start_Click(object sender, RoutedEventArgs e)
         {
-            // обнуляем счет
+            poisoningTimer.Stop();
+            // обнуляем счёт
             score = 0;
             // обнуляем змею
             snake.Clear();
-            // очищаем канвас
+            // очищаем canvas
             canvas1.Children.Clear();
             // скрываем надпись "Game Over"
             tbGameOver.Visibility = Visibility.Hidden;
-            
-            // добавляем поле на канвас
+            // скрываем кнопку "start"
+            btn_start.Visibility = Visibility.Hidden;
+
+            // скрываем шкалу прогресса
+            progress_bar.Visibility = Visibility.Hidden;
+            lbl.Visibility = Visibility.Hidden;
+
+            // скрываем текст-предупреждение об отравлении
+            lbl_poison.Visibility = Visibility.Hidden;
+
+            // добавляем поле на canvas
             canvas1.Children.Add(field.image);
-            // создаем новое яблоко и добавлем его
+
+            // создаем новое яблоко и добавляем его
             apple = new Apple(snake);
             canvas1.Children.Add(apple.image);
+            
+            // создаем новое отравленное яблоко и добавляем его
+            poisoned_apple_3 = new Poisoned_Apple_3(snake);
+            canvas1.Children.Add(poisoned_apple_3.image);
+
             // создаем голову
             head = new Head();
             snake.Add(head);
             canvas1.Children.Add(head.image);
-            
-            //запускаем таймер
+
+            //запускаем таймер движения
             moveTimer.Start();
+ 
             UpdateField();
 
         }
@@ -226,11 +336,12 @@ namespace Snake
             }
         }
 
+        //создаём клас "яблоко"
         public class Apple : PositionedEntity
         {
             List<PositionedEntity> m_snake;
             public Apple(List<PositionedEntity> s)
-                : base(0, 0, 40, 40, "pack://application:,,,/Resources/fruit.png")
+                : base(0, 0, 40, 40, "pack://application:,,,/Resources/apple1.png")
             {
                 m_snake = s;
                 move();
@@ -243,6 +354,40 @@ namespace Snake
                 {
                     x = rand.Next(13) * 40 + 40;
                     y = rand.Next(13) * 40 + 40;
+                    bool overlap = false;
+                    foreach (var p in m_snake)
+                    {
+                        if (p.x == x && p.y == y)
+                        {
+                            overlap = true;
+                            break;
+                        }
+                    }
+                    if (!overlap)
+                        break;
+                } while (true);
+
+            }
+        }
+
+        //создаём класс "отравленное яблоко 3"
+        public class Poisoned_Apple_3 : PositionedEntity
+        {
+            List<PositionedEntity> m_snake;
+            public Poisoned_Apple_3(List<PositionedEntity> s)
+                : base(0, 240, 40, 40, "pack://application:,,,/Resources/apple1.png")
+            {
+                m_snake = s;
+                move();
+            }
+
+            public override void move()
+            {
+                Random rand = new Random();
+                do
+                {
+                    x = rand.Next(12) * 40 + 40;
+                    y = rand.Next(12) * 40 + 40;
                     bool overlap = false;
                     foreach (var p in m_snake)
                     {
@@ -278,7 +423,7 @@ namespace Snake
             }
 
             public Head()
-                : base(280, 280, 40, 40, "pack://application:,,,/Resources/head.png")
+                : base(280, 280, 40, 40, "pack://application:,,,/Resources/head_snake.png")
             {
                 image.RenderTransformOrigin = new Point(0.5, 0.5);
                 m_direction = Direction.NONE;
@@ -308,7 +453,7 @@ namespace Snake
         {
             PositionedEntity m_next;
             public BodyPart(PositionedEntity next)
-                : base(next.x, next.y, 40, 40, "pack://application:,,,/Resources/body.png")
+                : base(next.x, next.y, 40, 40, "pack://application:,,,/Resources/body_snake.png")
             {
                 m_next = next;
             }
